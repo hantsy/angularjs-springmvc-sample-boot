a84f2ce1391b85d9a93d138dcbe62b8bad5bad7d
hantsy <hantsy@gmail.com>
2014-09-18 22:59:40 +0800

    update the datasource configuration.

--- src/main/java/com/hantsylabs/restexample/springmvc/config/DataSourceConfig.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/config/DataSourceConfig.java
@@ -1,28 +1,37 @@
 package com.hantsylabs.restexample.springmvc.config;
 
 import javax.inject.Inject;
-
 import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
+/**
+ * Different configurations for different stages.
+ * 
+ * In development stage using an embedded database to get better performance.
+ * 
+ * In production, container managed DataSource is highly recommended.
+ * 
+ * @author hantsy
+ *
+ */
 @Configuration
 public class DataSourceConfig {
-    
+
     @Inject
     private Environment env;
 
     @Bean
     @Profile("dev")
     public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .build();
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
     }
 
     @Bean
@@ -34,6 +43,16 @@ public class DataSourceConfig {
         bds.setUsername(env.getProperty("db.username"));
         bds.setPassword(env.getProperty("db.password"));
         return bds;
+    }
+
+    @Bean
+    @Profile("prod")
+    public DataSource prodDataSource() {
+        JndiObjectFactoryBean ds = new JndiObjectFactoryBean();
+        ds.setJndiName("");
+        ds.setCache(true);
+
+        return (DataSource) ds.getObject();
     }
 
 }
