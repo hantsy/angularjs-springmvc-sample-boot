b00ad7b73d52b4efc2348a244be4b452bb0c14f5
hantsy <hantsy@gmail.com>
2015-07-02 20:15:56 +0800

    clear the comments in java codes.

--- src/main/java/com/hantsylabs/restexample/springmvc/ApiErrors.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/ApiErrors.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.hantsylabs.restexample.springmvc;
 
 /**
  *
- * @author Hantsy Bai<hantsy@gmail.com> Bai<hantsy@gmail.com>
+ * @author Hantsy Bai<hantsy@gmail.com>
  */
 public class ApiErrors {
 
--- src/main/java/com/hantsylabs/restexample/springmvc/exception/InvalidRequestException.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/exception/InvalidRequestException.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.hantsylabs.restexample.springmvc.exception;
 
 import org.springframework.validation.BindingResult;
 
 /**
  *
- * @author Hantsy Bai<hantsy@gmail.com> Bai<hantsy@gmail.com>
+ * @author Hantsy Bai<hantsy@gmail.com>
  */
 public class InvalidRequestException extends RuntimeException {
 
