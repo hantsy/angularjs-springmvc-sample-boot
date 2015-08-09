63fdb66002a3a75a736702b47f3f4a9b8239b939
hantsy <hantsy@gmail.com>
2015-08-09 11:41:26 +0800

    clean the codes.

--- src/main/java/com/hantsylabs/restexample/springmvc/repository/UserRepository.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/repository/UserRepository.java
@@ -4,10 +4,8 @@ import com.hantsylabs.restexample.springmvc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+public interface UserRepository extends JpaRepository<User, Long>,
+        JpaSpecificationExecutor<User> {
 
-public interface UserRepository extends JpaRepository<User, Long> ,
-        JpaSpecificationExecutor<User>
-{
-
-	public User findByUsername(String username);
+    public User findByUsername(String username);
 }
