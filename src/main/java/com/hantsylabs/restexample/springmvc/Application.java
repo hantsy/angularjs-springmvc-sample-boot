b691d1a1f1fc7a1fbd34ae6007ae9fb330171df5
hantsy <hantsy@gmail.com>
2016-05-01 13:46:36 +0800

    replace security configuration with Spring Security 4.1 new features.

--- src/main/java/com/hantsylabs/restexample/springmvc/Application.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/Application.java
@@ -86,11 +86,7 @@ public class Application {
         return builder;
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        return passwordEncoder;
-    }
+   
 
 
 //    public WebSecurityConfigurerAdapter securityConfig(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -154,69 +150,112 @@ public class Application {
         }
 
     }
-
-    @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-
-        private final UserRepository userRepository;
-        private final PasswordEncoder passwordEncoder;
-
-        //@Inject
-        ApplicationSecurity(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-            this.userRepository = userRepository;
-            this.passwordEncoder = passwordEncoder;
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-            http
-                .authorizeRequests()
-                .antMatchers("/api/signup", "/api/users/username-check")
-                .permitAll()
-                .and()
-                    .authorizeRequests()
-                    .regexMatchers(HttpMethod.GET, "^/api/users/[\\d]*(\\/)?$").authenticated()
-                    .regexMatchers(HttpMethod.GET, "^/api/users(\\/)?(\\?.+)?$").hasRole("ADMIN")
-                    .regexMatchers(HttpMethod.DELETE, "^/api/users/[\\d]*(\\/)?$").hasRole("ADMIN")
-                    .regexMatchers(HttpMethod.POST, "^/api/users(\\/)?$").hasRole("ADMIN")
-                .and()
-                    .authorizeRequests()
-                    .antMatchers("/api/**").authenticated()
-                .and()
-                    .authorizeRequests()
-                    .anyRequest().permitAll()
-                .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                    .httpBasic()
-                .and()
-                    .csrf()
-                    .disable();
-        // @formatter:on
-        }
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth)
-                throws Exception {
-            auth
-                    .userDetailsService(new SimpleUserDetailsServiceImpl(userRepository))
-                    .passwordEncoder(passwordEncoder);
-        }
-
-        @Bean
-        @Override
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
-
-        @Bean
-        @Override
-        public UserDetailsService userDetailsServiceBean() throws Exception {
-            return super.userDetailsServiceBean();
-        }
-
+    
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder;
     }
+    
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return new SimpleUserDetailsServiceImpl(userRepository);
+    }
+    
+    @Bean
+    public WebSecurityConfigurerAdapter securityConfig(){
+        return new WebSecurityConfigurerAdapter() {
+            @Override
+            protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+                http
+                    .authorizeRequests()
+                    .antMatchers("/api/signup", "/api/users/username-check")
+                    .permitAll()
+                    .and()
+                        .authorizeRequests()
+                        .regexMatchers(HttpMethod.GET, "^/api/users/[\\d]*(\\/)?$").authenticated()
+                        .regexMatchers(HttpMethod.GET, "^/api/users(\\/)?(\\?.+)?$").hasRole("ADMIN")
+                        .regexMatchers(HttpMethod.DELETE, "^/api/users/[\\d]*(\\/)?$").hasRole("ADMIN")
+                        .regexMatchers(HttpMethod.POST, "^/api/users(\\/)?$").hasRole("ADMIN")
+                    .and()
+                        .authorizeRequests()
+                        .antMatchers("/api/**").authenticated()
+                    .and()
+                        .authorizeRequests()
+                        .anyRequest().permitAll()
+                    .and()
+                        .sessionManagement()
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                        .httpBasic()
+                    .and()
+                        .csrf()
+                        .disable();
+            // @formatter:on
+            }
+        };
+    }
+
+//    @Configuration
+//    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+//
+//        private final UserRepository userRepository;
+//        private final PasswordEncoder passwordEncoder;
+//
+//        //@Inject
+//        ApplicationSecurity(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//            this.userRepository = userRepository;
+//            this.passwordEncoder = passwordEncoder;
+////        }
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//        // @formatter:off
+//            http
+//                .authorizeRequests()
+//                .antMatchers("/api/signup", "/api/users/username-check")
+//                .permitAll()
+//                .and()
+//                    .authorizeRequests()
+//                    .regexMatchers(HttpMethod.GET, "^/api/users/[\\d]*(\\/)?$").authenticated()
+//                    .regexMatchers(HttpMethod.GET, "^/api/users(\\/)?(\\?.+)?$").hasRole("ADMIN")
+//                    .regexMatchers(HttpMethod.DELETE, "^/api/users/[\\d]*(\\/)?$").hasRole("ADMIN")
+//                    .regexMatchers(HttpMethod.POST, "^/api/users(\\/)?$").hasRole("ADMIN")
+//                .and()
+//                    .authorizeRequests()
+//                    .antMatchers("/api/**").authenticated()
+//                .and()
+//                    .authorizeRequests()
+//                    .anyRequest().permitAll()
+//                .and()
+//                    .sessionManagement()
+//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                    .httpBasic()
+//                .and()
+//                    .csrf()
+//                    .disable();
+//        // @formatter:on
+//        }
+//
+//        @Override
+//        protected void configure(AuthenticationManagerBuilder auth)
+//                throws Exception {
+//            auth
+//                    .userDetailsService(new SimpleUserDetailsServiceImpl(userRepository))
+//                    .passwordEncoder(passwordEncoder);
+//        }
+//
+//        @Bean
+//        @Override
+//        public AuthenticationManager authenticationManagerBean() throws Exception {
+//            return super.authenticationManagerBean();
+//        }
+//
+//       
+//
+//    }
 }
