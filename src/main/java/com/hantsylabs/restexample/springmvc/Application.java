2faa4dd7ba7385e1a16c56d5f025c22e64435c98
hantsy <hantsy@gmail.com>
2017-07-28 11:36:02 +0800

    fixes #11

--- src/main/java/com/hantsylabs/restexample/springmvc/Application.java
+++ src/main/java/com/hantsylabs/restexample/springmvc/Application.java
@@ -1,25 +1,13 @@
 package com.hantsylabs.restexample.springmvc;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:/static/");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("redirect:/index.html");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
